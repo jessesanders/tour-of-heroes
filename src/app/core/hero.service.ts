d48a71e83afcd2a4b893eb9b48f565653251c24d
Jesse Sanders <jesse.sanders@briebug.com>
2019-04-28 12:52:39 -0600

    removed extra map import

--- src/app/core/hero.service.ts
+++ src/app/core/hero.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Observable, BehaviorSubject } from 'rxjs';
-import { map } from 'rxjs/operators';
 
 import { Hero } from './hero';
 import { MessageService } from './message.service';
